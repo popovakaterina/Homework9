af1e60d47c130b6304130b23c0b0d8bfa144adfd
Popova Ekaterina <popova_katerina97@mail.ru>
2022-09-09 20:46:21 +0500

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N
+void Array(int[] array) // Объявление пустого масисва 
+{
+}
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nМассив натуральных чисел: ");
+for (int i = M; i <= N; i++) 
+{
+    if (i>0)
+    {
+       Console.Write( i + " " ); 
+    }   
+}
+
