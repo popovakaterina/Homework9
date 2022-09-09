dae777b23963ebb417eca716bef32e9af80700ee
Popova Ekaterina <popova_katerina97@mail.ru>
2022-09-09 21:01:31 +0500

    Task 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Array(int[] array) // Объявление пустого масисва 
+{
+}
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+for (int i = M; i <= N; i++) 
+{
+    if (i>0)
+    {
+        summ = summ+i;
+    }      
+}
+Console.WriteLine("Сумма натуральных элементов массива = " +summ);
